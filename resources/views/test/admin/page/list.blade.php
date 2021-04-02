b1ec8e1fa5fb1705cad44343a90b460debfbd744
Arkadii Vodolazskyi <jswift168@gmail.com>
2021-04-02 18:13:01 +0300

    Pages styles.

--- resources/views/test/admin/page/list.blade.php
+++ resources/views/test/admin/page/list.blade.php
@@ -1,22 +1,55 @@
 <x-test-admin-app-layout>
     @livewire('navigation-menu')
 
-    @foreach ($items as $item)
-        <div class="page_item">
-            <a href="page?id={{ $item->id }}" class="item_title">
-                {{ $item->slug }}
-            </a>
-            <a href="page?id={{ $item->id }}" class="btn">
-                edit
-            </a>
-            <a href="/{{ $item->slug }}" class="btn">
-                view
-            </a>
-            <a href="delete_page?id={{ $item->id }}" class="item_delete">
-                delete
-            </a>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="my-12 shadow-xl">
+            <div class="bg-white p-6 sm:px-12">
+                <table class="w-full pages mx-auto border-collapse">
+                    <thead>
+                        <tr>
+                            <th>Название страницы</th>
+                            <th>URL</th>
+                            <th colspan="3">Управление</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($items as $item)
+                            <tr class="px-3 py-4 font-semibold">
+                                <td>
+                                    {{ $item->title }}
+                                </td>
+                                <td>
+                                    {{ $item->slug }}
+                                </td>
+                                <td>
+                                    <a href="/{{ $item->slug }}" class="btn text-center px-3 py-1.5 text-white bg-gray-400 hover:bg-gray-600 hover:shadow-md">
+                                        Просмотреть
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="page?id={{ $item->id }}" class="btn text-center px-3 py-1.5 text-white bg-blue-400 hover:bg-blue-600 hover:shadow-md">
+                                        Редактировать
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="delete_page?id={{ $item->id }}" class="btn text-center px-3 py-1.5 text-white bg-red-400 hover:bg-red-800 hover:shadow-md">
+                                        Удалить
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        <tr>
+                            <td colspan="5" class="p-0 hover:bg-blue-200">
+                                <a href="/admin/page" class="block w-full h-full">Создать новую страницу</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    @endforeach
+    </div>
 
-    <a href="/admin/page">Добавить новую страницу</a>
+
+
+
 </x-test-admin-app-layout>
--- resources/views/test/admin/user/list.blade.php
+++ resources/views/test/admin/user/list.blade.php
@@ -1,23 +1,33 @@
 <x-test-admin-app-layout>
-    <h1>Users</h1>
     @if ($items->isNotEmpty())
-        <table>
-            <tr>
-                <td>name</td>
-                <td>email</td>
-                <td>roles</td>
-                <td>actions</td>
-            </tr>
-            @foreach ($items as $item)
+        <table class="w-full pages mx-auto border-collapse">
+            <thead>
                 <tr>
-                    <td>{{ $item->name }}</td>
-                    <td>{{ $item->email }}</td>
-                    <td>{{ $item->roles->map(function($item) {
-                        return $item->name;
-                    })->join(', ') }}</td>
-                    <td><a href="user?id={{ $item->id }}">edit</a> | <a href="delete_user?id={{ $item->id }}">delete</a></td>
+                    <th>name</th>
+                    <th>email</th>
+                    <th>roles</th>
+                    <th>actions</th>
                 </tr>
-            @endforeach
+            </thead>
+            <tbody>
+                @foreach ($items as $item)
+                    <tr class="px-3 py-4 font-semibold">
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->email }}</td>
+                        <td>{{ $item->roles->map(function($item) {
+                            return $item->name;
+                        })->join(', ') }}</td>
+                        <td>
+                            <a href="user?id={{ $item->id }}" class="btn text-center px-3 py-1.5 text-white bg-blue-400 hover:bg-blue-600 hover:shadow-md">
+                                Редактировать
+                            </a>
+                            <a href="delete_user?id={{ $item->id }}" class="btn text-center px-3 py-1.5 text-white bg-red-400 hover:bg-red-800 hover:shadow-md">
+                                Удалить
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
         </table>
     @endif
 </x-test-admin-app-layout>
